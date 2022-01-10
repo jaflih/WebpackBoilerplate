11368bdc72875271822d2c355bb9406ad7b5d4b1
jihaneh <jijih@hotmail.fr>
2022-01-10 16:33:33 +0100

    Add server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
